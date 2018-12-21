a05870cf43c15125f0161589c1c9d903c0cee461
Matt McFarland <contact@mattmcfarland.com>
2018-12-21 01:33:24 -0500

    add some console things

--- src/bin.ts
+++ src/bin.ts
@@ -1,13 +1,4 @@
-require("yargs")
-  .command(
-    "use [path]",
-    "path to your generation xml",
-    (yargs: any) => {},
-    (argv: any) => {
-      if (argv.verbose) console.info(`start server on :${argv.port}`);
-    }
-  )
-  .option("verbose", {
-    alias: "v",
-    default: false
-  }).argv;
+#!/usr/bin/env node
+import Yargs from "yargs";
+
+const argv = Yargs.argv;

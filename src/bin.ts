82e392f886b980822b8db1fb92f053e4a640c38a
Matt McFarland <contact@mattmcfarland.com>
2018-12-21 01:17:01 -0500

    add prettier

--- src/bin.ts
+++ src/bin.ts
@@ -1,16 +1,13 @@
-require('yargs') // eslint-disable-line
-  .command('serve [port]', 'start the server', (yargs: any) => {
-    yargs
-      .positional('port', {
-        describe: 'port to bind on',
-        default: 5000
-      })
-  }, (argv: any) => {
-    if (argv.verbose) console.info(`start server on :${argv.port}`)
-    
-  })
-  .option('verbose', {
-    alias: 'v',
+require("yargs")
+  .command(
+    "use [path]",
+    "path to your generation xml",
+    (yargs: any) => {},
+    (argv: any) => {
+      if (argv.verbose) console.info(`start server on :${argv.port}`);
+    }
+  )
+  .option("verbose", {
+    alias: "v",
     default: false
-  })
-  .argv
+  }).argv;

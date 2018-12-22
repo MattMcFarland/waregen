2173e42c930e6b91c02d0003e79bf6251ce29175
Matt McFarland <contact@mattmcfarland.com>
2018-12-22 16:40:31 -0500

    update ware test

--- src/lib/entities/Ware.spec.ts
+++ src/lib/entities/Ware.spec.ts
@@ -2,10 +2,16 @@ import { Ware } from "./Ware";
 
 describe("Ware Entity", () => {
   const ware = new Ware();
-  test("constructor with no options", () => {
-    expect(ware.toXml()).toBe('<ware id="" name="" transport="" volume=""/>');
-    expect(ware.toJson()).toBe(
-      '{"Attributes":{"id":"","name":"","transport":"","volume":1},"Children":{}}'
-    );
+  describe("constructor with no options", () => {
+    test("toXml()", () => {
+      expect(ware.toXml()).toBe(
+        '<ware id="" name="" transport="" volume="1"/>'
+      );
+    });
+    test("toJson()", () => {
+      expect(ware.toJson()).toBe(
+        '{"Attributes":{"id":"","name":"","transport":"","volume":1}}'
+      );
+    });
   });
 });

c54f5f44c183c5e3e45bc7dfa33fbba91e34d61a
Matt McFarland <contact@mattmcfarland.com>
2018-12-25 05:02:56 -0500

    add tests and tune asset ware prod macro

---
+++ src/lib/entities/AssetWareProdMacro.spec.ts
@@ -0,0 +1,48 @@
+import { AssetWareProdMacro } from "./AssetWareProdMacro";
+
+describe("AssetWareProdMacro", () => {
+  describe("using import", () => {
+    const xmlString = `
+    <?xml version="1.0" encoding="utf-8"?>
+    <!--Exported by: Florian (192.168.3.134) at 23.11.2018_17-23-25-->
+    <macros>
+      <macro name="prod_arg_foodrations_macro" class="production">
+        <component ref="prod_arg_foodrations" />
+        <properties>
+          <identification name="{20104,13401}" shortname="{20104,13403}" makerrace="argon" description="{20104,13402}" />
+          <build>
+            <sets>
+              <set ref="factory" />
+              <set ref="headquarters_player" />
+            </sets>
+          </build>
+          <explosiondamage value="10000" />
+          <hull max="133000" />
+          <secrecy level="2" />
+          <production wares="foodrations">
+            <queue ware="foodrations" method="argon" />
+          </production>
+          <workforce max="90" />
+        </properties>
+      </macro>
+    </macros>    
+    `;
+    test("has correct name", async () => {
+      const macro = await new AssetWareProdMacro().import(xmlString);
+      expect(macro.name).toBe("prod_arg_foodrations_macro");
+    });
+    test("can change name", async () => {
+      const macro = await new AssetWareProdMacro().import(xmlString);
+      macro.name = "foo";
+      expect(macro.__xmlDef.macro[0].Attributes.name).toBe("foo");
+    });
+    test("toXml()", async () => {
+      const macro = await new AssetWareProdMacro().import(xmlString);
+      expect(macro.toXml()).toMatchSnapshot();
+    });
+    test("tJson()", async () => {
+      const macro = await new AssetWareProdMacro().import(xmlString);
+      expect(macro.toJson()).toMatchSnapshot();
+    });
+  });
+});
--- src/lib/entities/AssetWareProdMacro.ts
+++ src/lib/entities/AssetWareProdMacro.ts
@@ -1,8 +1,17 @@
 import { X4Entity, X4EntityType } from "./X4Entity";
 import { Macros } from "@@/XMLTypes/X4WareMacro";
+import idx from "idx";
 
 export class AssetWareProdMacro extends X4Entity<Macros> {
-  constructor(xmlObj: any) {
+  get name() {
+    return idx(this.xmlDef, _ => _.macro[0].Attributes.name) || "";
+  }
+  set name(v: string) {
+    if (this.xmlDef.macro && this.xmlDef.macro[0]) {
+      this.xmlDef.macro[0].Attributes.name = v;
+    }
+  }
+  constructor(xmlObj: any = {}) {
     super(X4EntityType.ASSET_WARE_MACRO, "macros", xmlObj);
   }
 }

e3f1a32b14e7e76432d5f9c99f083b55124a9d5e
Matt McFarland <contact@mattmcfarland.com>
2018-12-26 04:31:10 -0500

    implement mock defaults util for tests

--- src/lib/generator/helpers/__tests__/util.ts
+++ src/lib/generator/helpers/__tests__/util.ts
@@ -1,4 +1,4 @@
-import { Resolver } from "@@/utils";
+import { Resolver, clone, range } from "@@/utils";
 import { DefaultsEntity, AddwareEntity } from "@@/XMLTypes/X4WareGenXML";
 
 export interface IMockGeneratorArgs {
@@ -21,9 +21,65 @@ export const mockResolver = (): Resolver =>
     "test_unpacked"
   );
 
-export const mockDefaults = (): DefaultsEntity => ({});
+export const mockDefaults = (): DefaultsEntity => ({
+  ware: [defaultWare()]
+});
 
 export const mockAddwares = (count: number): AddwareEntity[] => {
-  let i = 0;
-  while (i <= count) {}
+  return range(count).map(
+    (i: number): AddwareEntity => ({
+      Attributes: {
+        baskets: `[test_basket_${i}]`,
+        cloneProductionModuleFrom: `prod_module_macro_${i}`,
+        id: `test_ware_${i}`
+      }
+    })
+  );
 };
+
+const defaultWare = () =>
+  clone({
+    Attributes: {
+      id: "default",
+      name: "default",
+      transport: "container",
+      volume: 1
+    },
+    price: [
+      {
+        Attributes: {
+          min: 1,
+          average: 1,
+          max: 1
+        }
+      }
+    ],
+    production: [
+      {
+        Attributes: {
+          time: 10,
+          amount: 1,
+          method: "default",
+          name: "{20206, 101}"
+        },
+        primary: [
+          {
+            ware: [{ Attributes: { ware: "energycells", amount: 50 } }]
+          }
+        ],
+        effects: [
+          {
+            effect: [
+              {
+                Attributes: { type: "efficiency", product: 1 }
+              }
+            ]
+          }
+        ]
+      }
+    ],
+    container: [{ Attributes: { ref: "sm_gen_pickup_container_01_macro" } }],
+    icon: [
+      { Attributes: { active: "ware_default", video: "ware_noicon_macro" } }
+    ]
+  });

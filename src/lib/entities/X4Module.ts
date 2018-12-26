fe1aad1aebae99146722cb452963a4c109d90f19
Matt McFarland <contact@mattmcfarland.com>
2018-12-25 22:38:28 -0500

    add module

---
+++ src/lib/entities/X4Module.ts
@@ -0,0 +1,54 @@
+import idx from "idx";
+import { X4Entity, X4EntityType } from "./X4Entity";
+import { ModuleEntity } from "@@/XMLTypes/X4LibraryModules";
+import { clone } from "lodash";
+
+export interface ModuleInterface {
+  id: string;
+  ware: string;
+}
+
+export class X4Module extends X4Entity<ModuleEntity> {
+  get id() {
+    return this.xmlDef.Attributes.id;
+  }
+  get ware() {
+    return idx(this.xmlDef, _ => _.category[0].Attributes.ware) || "";
+  }
+  set ware(v: string) {
+    const id = this.xmlDef.Attributes.id;
+    this.xmlDef = baseModule(id, v);
+  }
+  set id(v: string) {
+    const ware = idx(this.xmlDef, _ => _.category[0].Attributes.ware) || "";
+    this.xmlDef = baseModule(v, ware);
+  }
+  constructor(id: string, ware: string) {
+    super(X4EntityType.LIBRARY_MODULE, "module", baseModule(id, ware));
+  }
+}
+
+const baseModule: (id: string, ware: string) => ModuleEntity = (
+  id: string,
+  ware: string
+) =>
+  clone({
+    Attributes: { id, group: id },
+    category: [
+      {
+        Attributes: {
+          ware,
+          tags: "[production, module]",
+          race: "[argon, paranid, teladi]",
+          faction:
+            "[argon, paranid, teladi, antigone, holyorder, ministry, hatikvah, alliance, scaleplate]"
+        }
+      }
+    ],
+    compatibilities: [
+      {
+        production: [{ Attributes: { ware, chance: 65 } }],
+        limits: [{ Attributes: { production: 1 } }]
+      }
+    ]
+  });
---
+++ src/lib/entities/X4ModuleCollection.ts
@@ -0,0 +1,66 @@
+import idx from "idx";
+import { X4Entity, X4EntityType } from "./X4Entity";
+import {
+  ModuleEntity,
+  Modules as ModuleEntities
+} from "@@/XMLTypes/X4LibraryModules";
+import { X4Module } from "./X4Module";
+export interface ModulesIndex {
+  [key: string]: X4Module;
+}
+export class ModuleCollection extends X4Entity<ModuleEntities> {
+  private modulesIndex: ModulesIndex = {};
+  protected onImport(_xmlDef: ModuleEntities) {
+    delete this.modulesIndex;
+    this.modulesIndex = {};
+    delete this.xmlDef;
+    this.xmlDef = { module: [] };
+
+    if (Array.isArray(_xmlDef.module)) {
+      _xmlDef.module.forEach((moduleEntity: ModuleEntity) => {
+        const wareName =
+          idx(moduleEntity, _ => _.category[0].Attributes.ware) || "";
+        const id = idx(moduleEntity, _ => _.Attributes.id) || "";
+        const mod = new X4Module(id, wareName);
+        this.add(mod);
+      });
+    }
+  }
+  add = (mod: X4Module) => {
+    if (this.modulesIndex[mod.id]) {
+      throw new Error(`mod ${mod.id} already exists, did you mean replace?`);
+    }
+    this.modulesIndex[mod.id] = mod;
+    if (Array.isArray(this.xmlDef.module)) {
+      this.xmlDef.module = [...this.xmlDef.module, mod.__xmlDef];
+    }
+  };
+  remove = (id: string) => {
+    if (!this.modulesIndex[id]) {
+      throw new Error(`cant remove ware ${id} because it does not exist`);
+    }
+    delete this.modulesIndex[id];
+    if (Array.isArray(this.xmlDef.module)) {
+      this.xmlDef.module = this.xmlDef.module.filter(
+        modEntity => modEntity.Attributes.id !== id
+      );
+    }
+  };
+  replace = (mod: X4Module) => {
+    this.remove(mod.id);
+    this.add(mod);
+  };
+  extract = (id: string) => {
+    return this.modulesIndex[id];
+  };
+  constructor(optionalModuleList?: X4Module[]) {
+    super(X4EntityType.LIBRARY_MODULES, "modules", {
+      module: []
+    });
+    if (Array.isArray(optionalModuleList)) {
+      optionalModuleList.forEach((mod: X4Module) => {
+        this.add(mod);
+      });
+    }
+  }
+}

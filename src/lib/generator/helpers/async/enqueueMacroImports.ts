2463958b45a94b36c13ae588d9997b1b4e599837
Matt McFarland <contact@mattmcfarland.com>
2018-12-29 04:32:57 -0500

    implement ware macro generation

--- src/lib/generator/helpers/async/enqueueMacroImports.ts
+++ src/lib/generator/helpers/async/enqueueMacroImports.ts
@@ -11,16 +11,19 @@ export default function enqueueMacroImports(
   options: GeneratorOptions
 ) {
   const promises = config.addwaresList.map(addware => {
-    console.log(options);
     return new Promise(async (resolve, reject) => {
       const ids = new IdRoster(addware.Attributes.id, config.modPrefix);
       const sourceMacro = addware.Attributes.cloneProductionModuleFrom;
-      const sourceMacroPath = pathBuilder(config, addware.Attributes.id)
+      const sourceMacroPath = pathBuilder(
+        config,
+        addware.Attributes.cloneProductionModuleFrom
+      )
         .dirFullGamepath()
         .dirUnpackedPath()
         .dirProductionMacros()
         .append(sourceMacro + ".xml")
         .resolve();
+
       const parser = new Parser();
       const data = await parser.parseFile(sourceMacroPath);
 
@@ -29,17 +32,21 @@ export default function enqueueMacroImports(
       cloneMacro.name = addware.Attributes.id;
       cloneMacro.shortname = addware.Attributes.id;
       cloneMacro.production = ids.ware;
+
       const destMacroPathDir = pathBuilder(config, addware.Attributes.id)
         .dirFullGamepath()
         .dirModPath()
         .dirProductionMacros()
         .resolve();
-      Mkdirp.sync(destMacroPathDir);
+
       const destMacroFile = pathBuilder(config, addware.Attributes.id)
         .dirFullGamepath()
         .dirModPath()
         .fileProductionMacroXML("xml")
         .resolve();
+
+      Mkdirp.sync(destMacroPathDir);
+
       FS.writeFile(destMacroFile, cloneMacro.toXml(), err => {
         if (err) return reject(err);
         return resolve(destMacroFile);
---
+++ src/lib/generator/helpers/async/enqueueMacroWareImports.ts
@@ -0,0 +1,41 @@
+import { GeneratorConfig, GeneratorOptions } from "@@/generator";
+import { AssetWareMacro } from "@@/entities";
+import pathBuilder from "@@/utils/PathBuilder";
+
+import { IdRoster } from "@@/utils/IdRoster";
+import Mkdirp from "mkdirp";
+import FS from "fs";
+
+export default function enqueueMacroWareImports(
+  config: GeneratorConfig,
+  options: GeneratorOptions
+) {
+  const promises = config.addwaresList.map(addware => {
+    return new Promise(async (resolve, reject) => {
+      const ids = new IdRoster(addware.Attributes.id, config.modPrefix);
+
+      const wareMacro = await new AssetWareMacro(ids.wareMacro, "ware_default");
+
+      const destMacroPathDir = pathBuilder(config, addware.Attributes.id)
+        .dirFullGamepath()
+        .dirModPath()
+        .dirWareMacro()
+        .resolve();
+
+      const destMacroFile = pathBuilder(config, addware.Attributes.id)
+        .dirFullGamepath()
+        .dirModPath()
+        .fileWareMacroXML("xml")
+        .resolve();
+
+      Mkdirp.sync(destMacroPathDir);
+
+      FS.writeFile(destMacroFile, wareMacro.toXml(), err => {
+        if (err) return reject(err);
+        return resolve(destMacroFile);
+      });
+    });
+  });
+
+  return promises;
+}

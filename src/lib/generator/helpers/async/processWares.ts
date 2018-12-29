34227a22ccaec92b80807d6cf7cfd35b49906609
Matt McFarland <contact@mattmcfarland.com>
2018-12-29 04:32:39 -0500

    move processWares to async since its async

--- src/lib/generator/helpers/async/processWares.ts
+++ src/lib/generator/helpers/async/processWares.ts
@@ -1,4 +1,3 @@
-import { DefaultsEntity, AddwareEntity } from "@@/XMLTypes/X4WareGenXML";
 import {
   createBasketCollection,
   createIcons,
@@ -6,33 +5,21 @@ import {
   createModuleGroups,
   createModules,
   createWareCollection
-} from "./";
+} from "..";
 
 import {
   enqueueMacroImports,
   enqueueIconTextureImports,
-  enqueueFileWrites
-} from "./async";
+  enqueueFileWrites,
+  enqueueMacroWareImports
+} from ".";
 
-import {
-  WareCollection,
-  IndexHash,
-  IconCollection,
-  BasketCollection,
-  ModuleGroupsCollection,
-  AssetWareMacro,
-  AssetWareProdMacro
-} from "@@/entities";
 import { GeneratorConfig, GeneratorOptions } from "@@/generator";
 
 export default async function processWares(
   config: GeneratorConfig,
   options: GeneratorOptions
 ) {
-  // const iconCollection = new IconCollection();
-  // const moduleGroupsCollection = new ModuleGroupsCollection();
-  // const importAssetWareMacros: Promise<AssetWareMacro>[] = [];
-  // const importAssetWareProdMacros: Promise<AssetWareProdMacro>[] = [];
   /* 
     Create all of the X4Entity types, which will be used later
     for writing to the file system.
@@ -45,28 +32,54 @@ export default async function processWares(
     by adding additional logic to a single loop. so we'll just loop over 
     the constant as much as we need to) ;)  
   */
+
   /* Create the Macros Index */
   const macrosIndex = createMacroIndex(config);
+
   /* Create the Icon Collection */
   const iconCollection = createIcons(config);
+
   /* Create Ware Entity and add it to Collection */
   const wareCollection = createWareCollection(config);
+
   /* Create "Module Groups" Collection */
   const moduleGroupsCollection = createModuleGroups(config);
+
   /* Create Modules Collection */
   const modulesCollection = createModules(config);
+
   /* Create the Basket Collection */
   const basketCollection = createBasketCollection(config);
 
-  /* Create Array of Promises to import Asset Ware Macros and asset Production Macros */
-  const importWareAssets = enqueueMacroImports(config, options);
+  /* Create Array of Promises to import asset Production Macros */
+  const importWareProdAssets = enqueueMacroImports(config, options);
+
+  /* Create Array of Promises to create asset Ware Macros */
+
+  const importWareAssets = enqueueMacroWareImports(config, options);
 
   /* Arry of Promises for gunzipping icons from unpacked to their destination*/
-  // todo const importIconTextures = enqueueIconTextureImports(config, options)
-  /* Array of Promises for writing the files */
-  // todo consider using Object.entries over a setup object instead.
+  const importIconTextures = enqueueIconTextureImports(config, options);
 
-  // todo const fileWrites = enqueueFileWrites([ macrosIndex, iconCollection, wareCollection, basketCollection, moduleGroupsCollection, modulesCollection ], config, options)
+  /* Array of Promises for writing the files */
+  const fileWrites = enqueueFileWrites(
+    [
+      macrosIndex,
+      iconCollection,
+      wareCollection,
+      basketCollection,
+      moduleGroupsCollection,
+      modulesCollection
+    ],
+    config,
+    options
+  );
+
   /* return the promises to be resolved */
-  // todo return Promise.all([...fileWrites, ...importWareAssets, ...importIconTextures])
+  return Promise.all([
+    ...fileWrites,
+    ...importWareAssets,
+    ...importIconTextures,
+    ...importWareProdAssets
+  ]);
 }
